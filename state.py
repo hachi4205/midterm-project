207d7cb972346c79a18305165eebfdbd7fef2947
hachi <nguyenhachi4205@gmail.com>
2026-05-01 22:54:18 +0900

    game variables

---
+++ state.py
@@ -0,0 +1,15 @@
+player = {
+    "status": "배고픔",
+    "location": "연대앞 버스정류장",
+    "HP": 10,
+    "balance": 50000,
+    "inventory": [],
+    "quests": []
+}
+
+environment = {"time": 11}
+settings = {"difficulty": "보통"}
+input_history = []
+
+row = 6
+col = 0
